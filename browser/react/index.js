8ec0dda3674bc6f486deb1d13e1fd8b475b0a5bd
joaquin gaona <joaquinngaona@gmail.com>
2020-05-29 16:39:49 -0300

    primer commit

--- browser/react/index.js
+++ browser/react/index.js
@@ -1,1 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Main from './Main';
+
+
+
+ReactDOM.render(
+
+    <Main />,
+    document.getElementById('app') 
+    );
+
 console.log('Hello React');
